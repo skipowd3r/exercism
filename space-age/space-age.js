be31fadde0f1a801fdb074e5b014c5ab5b89e55e
Kate <kate.voorhes@gmail.com>
2019-05-23 12:21:04 -0400

    all tests passed

---
+++ space-age/space-age.js
@@ -0,0 +1,15 @@
+export const age = (planet, seconds) => {
+const earthYears = seconds/31557600;
+const planetYears = {
+  'earth': 1,
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'mars': 1.8808158,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus': 84.016846,
+  'neptune': 164.79132,
+};
+let result = earthYears/(planetYears[planet])
+return parseFloat(result.toFixed(2));
+}
